ad6699a16fa3aca414eb606eecf8f021a94229a5
spmburgess <spmburgess@gmail.com>
2019-06-11 14:08:47 -0600

    Added phrases and cleaned up code

--- SpeedTest/Program.cs
+++ SpeedTest/Program.cs
@@ -17,7 +17,16 @@ namespace SpeedTest
                 "A house divided against itself cannot stand.",
                 "A man who is his own lawyer has a fool for a client.",
                 "Abandon all hope ye who enter here.",
-                "Absence makes the heart grow fonder."
+                "Absence makes the heart grow fonder.",
+                "Jack of All Trades Master of None.",
+                "Everything But The Kitchen Sink.",
+                "Barking Up The Wrong Tree.",
+                "A Fool and His Money are Soon Parted.",
+                "What Am I, Chopped Liver?",
+                "You Can't Judge a Book By Its Cover",
+                "Every Cloud Has a Silver Lining.",
+                "You Can't Teach an Old Dog New Tricks.",
+                "Birds of a Feather Flock Together."
             };
 
             Random ranNum = new Random();
@@ -26,6 +35,7 @@ namespace SpeedTest
 
             bool runLoop = true;
 
+            // Rerun test as long as user has not quit
             while(runLoop)
             {
                 // Begin output
@@ -57,7 +67,6 @@ namespace SpeedTest
                     }
 
                     writenPhrases.Add(inString);
-                    //Console.WriteLine("You entered '{0}'", inString);
 
                     i++;
                 }
@@ -73,14 +82,13 @@ namespace SpeedTest
                 long timeDiff = DateTimeOffset.UtcNow.ToUnixTimeSeconds() - startTime;
                 double timeMins = (double)timeDiff / 60;
                 int wpm = (int)(totalWords / timeMins);
+
                 Console.WriteLine("WPM = {0}", wpm);
                 Console.WriteLine("Words = {0}", totalWords);
                 Console.WriteLine("Total time taken = {0} seconds", timeDiff);
                 Console.ReadKey();
                 Console.Clear();
             }
-
-
         }
     }
 }
